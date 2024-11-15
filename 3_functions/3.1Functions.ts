aa649869ce2e849f3f57e9ad7518007b1f6f0670
KaanCaman <106434723+KaanCaman@users.noreply.github.com>
2024-11-15 07:52:52 -0800

    addNumbers function add

--- 3_functions/3.1Functions.ts
+++ 3_functions/3.1Functions.ts
@@ -6,11 +6,20 @@ interface AddProps {
   b: number;
 }
 
-
 // define addTwoNumber Function
 function addTwoNumber(prop: AddProps): string {
   return `${prop.a + prop.b}`;
 }
 
+// define addNumbers
+function addNumbers(...params: Array<number>): number {
+  let sum: number = 0;
+  for (let index = 0; index < params.length; index++) {
+    sum += params[index];
+  }
+  return sum;
+}
+
 const result = addTwoNumber({ a: 24, b: 24 });
-console.log(result); // 48
+const result2 = addNumbers(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+console.log(result, result2); // 48 , 45
